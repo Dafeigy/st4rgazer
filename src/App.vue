9deca86c3d21848c772042111a69a2c5993ac72c
Dafeigy <1070642765@qq.com>
2025-02-19 17:27:11 +0800

    <WIP>: Result rendered issues when avatar is not specific size.

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,21 @@ onUpdated(() => {
 onMounted(() => {
     Prism.highlightAll(); //切换菜单重新渲染
 })
-const imageUrl = '#';
 const searchThreshold = ref(0.75);
+
+const data = ref(null);
+
+const fetchData = async () => {
+  try {
+    
+    const response = await fetch('https://gs.cybercolyce.icu/test_search');
+    const result = await response.json();
+    data.value = result;
+    console.log(data.value)
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 </script>
 
 <template>
@@ -18,21 +31,19 @@ const searchThreshold = ref(0.75);
   <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
   <div id="sidebar" class="flex flex-col bg-primary w-[30vmin] h-full mx-2 font-display">
     <div id="logo"
-      class="font-display  flex w-full flex-col items-center justify-center text-center text-white font-display text-[4vmin] h-[5%] mt-2">
+      class="flex w-full flex-col items-center justify-center text-center text-white font-display text-[4vmin] h-[5%] mt-2">
       [ DEBUG ]
     </div>
 
     <div id="constructing" class="flex flex-col justify-center h-[95%] w-full items-center">
       <div id="debug" class="mt-[3%] font-display h-[80%] bg-second w-full overflow-scroll whitespace-nowrap language-javascript rounded-xl" style="scrollbar-color: transparent transparent; word-wrap: break-word;">
         <pre><code>// This is a debug window</code>
-<code>var a=1; var a=1; var a=1; var a=1; var a=1; var a=1;</code>
-<code>var a=1; var a=1; var a=1; var a=1; var a=1; var a=1;</code>
 <code>var a=1; var a=1; var a=1; var a=1; var a=1; var a=1;</code></pre>
         <pre>
 <code>// Searching with @0.86</code>
         </pre>
       </div>
-      <div id="others" class="font-display  w-full pt-4 h-[20%] w-full flex flex-col justify-center items-center">
+      <div id="others" class="font-display pt-4 h-[20%] w-full flex flex-col justify-center items-center">
         <h2 class="font-display text-white text-[3vmin] text-center">[ Settings ]</h2>
         <div class="flex text-white ">
           <div class="text-nowrap">Sim Threadhold:</div>
@@ -46,15 +57,15 @@ const searchThreshold = ref(0.75);
     class="bg-second w-[calc(100%-30vmin)] flex flex-col items-center h-[98%] justify-top rounded-2xl pt-[8%]">
     <h1 class="text-4xl font-display text-[#fff] text-nowrap">✨Which star to gaze today?</h1>
     <div id="author"
-      class="font-display rounded-lg h-[5%] w-[80vmin] text-md text-[#fff] flex justify-center items-center bg-primary mt-[0.5%] text-white">
-      <div class="text-[1.5vmin] text-center items-center flex justify-center">Made by<a href="" class="text-theme">&nbsp;@NUL4I </a>&nbsp;with:
+      class="font-display rounded-lg h-[5%] w-[80vmin] text-md flex justify-center items-center bg-primary mt-[0.5%] text-white">
+      <div class="text-[1.5vmin] text-center items-center flex justify-center">Maybe made by<a href="" class="text-theme">&nbsp;@NUL4I </a>&nbsp;with:
         [&nbsp;
         <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-fastapi-plain colored"></i></a>&nbsp;+&nbsp;
         <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-vuejs-plain colored"></i></a>&nbsp;+&nbsp;
         <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-c-plain colored"></i></a>&nbsp;+&nbsp;
         <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-tailwindcss-original colored"></i></a>&nbsp;+&nbsp;
         <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-wasm-original colored"></i></a>&nbsp;+&nbsp;
-        <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-github-original"></i></a>&nbsp;], deployed by <a href="">&nbsp;<i
+        <a href="" class="hover:-translate-y-1 hover:scale-120 transition duration-200 delay-50"><i class="devicon-github-original"></i></a>&nbsp;], deployed in <a href="">&nbsp;<i
             class="devicon-vercel-original"></i>Vercel</a>
       </div>
     </div>
@@ -87,33 +98,12 @@ const searchThreshold = ref(0.75);
           <div id="status" class="items-center justify-center">&leftarrow;Sync First</div>
         </div>
         <div id="search" class="flex flex-col items-center w-[10%] h-full justify-right">
-          <div id="searchbtn" class="cursor-pointer btn flex h-[90%] items-center w-full justify-center bg-primary transition delay-50 duration-300 ease-in-out hover:bg-theme rounded-md hover:text-[#fff]">search</div>
+          <div @click="fetchData" id="searchbtn" class="cursor-pointer btn flex h-[90%] items-center w-full justify-center bg-primary transition delay-50 duration-300 ease-in-out hover:bg-theme rounded-md hover:text-[#fff]">search</div>
         </div>
       </div>
     </div>
-    <div id="results" class="flex flex-col w-[80vmin] h-[50%] justify-top items-center py-5 ">
-      <Result 
-        title="NUL4I/StarGazer"
-        description="Woops. Nothing here yet."
-        :avatar_url="imageUrl"
-        :clone_url="imageUrl"
-        star=1
-        fork=1
-        update="T2024-12-12-09:00Z"
-        lang="Python"
-        sim=0.9897
-        />
-      <Result 
-        title="NUL4I/StarGazer"
-        description="Woops. Nothing here yet."
-        :avatar_url="imageUrl"
-        :clone_url="imageUrl"
-        star=1
-        fork=1
-        update="T2024-12-12-09:00Z"
-        lang="Python"
-        sim=0.9897
-        />  
+    <div id="results" class=" overflow-scroll scrollbar-color-[#3CB371] flex flex-col w-[80vmin] h-[50%] justify-top items-center py-1 my-2" style="scrollbar-color: transparent transparent; overflow-x: hidden;">
+      <Result v-for="each in data" :key="each.reponame" :item="each"/>
     </div>
   </div>
 </template>
